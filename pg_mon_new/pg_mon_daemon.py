54b6dadb3548fb11d46abb3e53f2b8d05e11994e
Arsen Movsesyan <arsen.movsesyan@gmail.com>
2013-09-24 10:43:11 -0700

    Fixed bug with duplicate time_id when multi hostcluster env

--- pg_mon_new/pg_mon_daemon.py
+++ pg_mon_new/pg_mon_daemon.py
@@ -59,13 +59,24 @@ class PgmonDaemon(Daemon):
 	conn=psycopg2.connect(settings.custom_dsn('pg_mon'))
 
 	while True:
+	    runtime_stat=False
+	    regular_stat=False
 	    current_time_minute=self._get_current_time('%M')
 	    logger.debug("Iteration started. Minute {0}".format(current_time_minute))
 	    if (current_time_minute % settings.runtime_stat_interval) == 0:
-		logger.debug("Iteration in runtime_stat_interval. Minute {0}".format(current_time_minute))
 		ltm_id=ltm.get_id()
 		if not ltm_id:
 		    continue
+		runtime_stat=True
+		if (current_time_minute % settings.regular_stat_interval) == 0:
+		    lt_id=lt.get_id()
+		    if not lt_id:
+			continue
+		    regular_stat=True
+
+
+	    if runtime_stat:
+		logger.debug("Iteration in runtime_stat_interval. Minute {0}".format(current_time_minute))
 		cur=conn.cursor()
 		try:
 		    cur.execute("SELECT id FROM host_cluster WHERE alive AND observable")
@@ -82,45 +93,43 @@ class PgmonDaemon(Daemon):
 			continue
 		    hc.runtime_stat(ltm_id)
 		    hc.discover_cluster_databases()
-		    if (current_time_minute % settings.regular_stat_interval) == 0:
+		    if regular_stat:
 			logger.debug("Iteration in regular_stat_interval. Minute {0}".format(current_time_minute))
-			lt_id=lt.get_id()
 			hc.stat(lt_id)
 		    for dbs in hc.get_dependants(True):
 			db_dsn=dbs['db_dsn']
 			dn=DatabaseName(conn,db_dsn,dbs['id'])
 			dn.runtime_stat(ltm_id)
 			dn.discover_schemas()
-			if (current_time_minute % settings.regular_stat_interval) == 0:
-			    dn.stat(lt_id)
-			    for sn in dn.get_dependants(True):
-				sn=SchemaName(conn,db_dsn,sn)
-				sn.discover_tables()
-				sn.discover_functions()
-				for tbl_id in sn.get_tables():
-				    tn=TableName(conn,db_dsn,tbl_id)
-				    tn.stat(lt_id)
-				    tn.va_stat(lt_id)
-				    tn.discover_indexes()
-				    for ind_id in tn.get_dependants():
-					ind=IndexName(conn,db_dsn,ind_id)
-					ind.stat(lt_id)
+			dn.stat(lt_id)
+			for sn in dn.get_dependants(True):
+			    sn=SchemaName(conn,db_dsn,sn)
+			    sn.discover_tables()
+			    sn.discover_functions()
+			    for tbl_id in sn.get_tables():
+				tn=TableName(conn,db_dsn,tbl_id)
+				tn.stat(lt_id)
+				tn.va_stat(lt_id)
+				tn.discover_indexes()
+				for ind_id in tn.get_dependants():
+				    ind=IndexName(conn,db_dsn,ind_id)
+				    ind.stat(lt_id)
 					
-				    tn.discover_toast()
-				    toast_id=tn.get_toast_id()
-				    if toast_id:
-					ttn=TableToastName(conn,db_dsn,toast_id)
-					ttn.stat(lt_id)
-					ttn.discover_index()
-					tin=IndexToastName(conn,db_dsn,ttn.get_tindex_id())
-					tin.stat(lt_id)
+				tn.discover_toast()
+				toast_id=tn.get_toast_id()
+				if toast_id:
+				    ttn=TableToastName(conn,db_dsn,toast_id)
+				    ttn.stat(lt_id)
+				    ttn.discover_index()
+				    tin=IndexToastName(conn,db_dsn,ttn.get_tindex_id())
+				    tin.stat(lt_id)
 					
-				if hc.get_track_function() != 'none':
-				    for fnc_id in sn.get_functions():
-					func=FunctionName(conn,db_dsn,fnc_id)
-					func.stat(lt_id)
-			lt.reset_id()
-		ltm.reset_id()
+			    if hc.get_track_function() != 'none':
+				for fnc_id in sn.get_functions():
+				    func=FunctionName(conn,db_dsn,fnc_id)
+				    func.stat(lt_id)
+		    lt.reset_id()
+	    ltm.reset_id()
 
 	    delay_to_next=60 * settings.runtime_stat_interval - self._get_current_time()+10
 	    logger.debug("Iteration Done. Minute {0} Next delay {1}".format(current_time_minute,delay_to_next))
